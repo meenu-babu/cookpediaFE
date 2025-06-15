111880595ea6514d7a6df89def734c4c2c8016cf
meenu <meenubabu273@gmail.com>
2025-06-16 00:40:52 +0530

    cookpedia design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Recipe } from './recipe/recipe';
+import { Profile } from './profile/profile';
+import { SavedRecipe } from './saved-recipe/saved-recipe';
+import { ViewRecipe } from './view-recipe/view-recipe';
+import { PageNotFound } from './page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // define path to load specific components
+    {path:'',component:Home},
+    {path:'about',component:About},
+    {path:'contact',component:Contact},
+    {path:'login',component:Login},
+    {path:'register',component:Register},
+    {path:'all-recipe',component:Recipe},
+    {path:'profile',component:Profile},
+    {path:'saved-recipe',component:SavedRecipe},
+    // BINDING ID DYNAMICALLY IN PATH
+    {path:'recipe/:id/view',component:ViewRecipe},
+    // manage path that page not found
+    // for that we use wild card path
+    {path:'**',component:PageNotFound}
+
+];
---
+++ src/app/contact/contact.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+
+@Component({
+  selector: 'app-contact',
+  imports: [Header,Footer],
+  templateUrl: './contact.html',
+  styleUrl: './contact.css'
+})
+export class Contact {
+
+}
