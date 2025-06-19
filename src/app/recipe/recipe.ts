53def89d61796d73e9f7d34d1e94858a2808e459
meenu <meenubabu273@gmail.com>
2025-06-19 15:52:36 +0530

    recipe filtering

--- src/app/recipe/recipe.ts
+++ src/app/recipe/recipe.ts
@@ -5,28 +5,62 @@ import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-recipe',
-  imports: [Header,Footer],
+  imports: [Header, Footer],
   templateUrl: './recipe.html',
-  styleUrl: './recipe.css'
+  styleUrl: './recipe.css',
 })
 export class Recipe {
-  allRecipes:any=[]
-  constructor(private api:Api){}
+  allRecipes: any = [];
+  allRecipesDummy: any = [];
+  cuisineArray: any = [];
+  mealsArray: any = [];
 
+  constructor(private api: Api) {}
 
   //ngOnInit() is a angular hook,it excecuted once after the component initialised
-  ngOnInit(){
-    this.getAllRecipes()
+  ngOnInit() {
+    this.getAllRecipes();
+  }
+
+  getAllRecipes() {
+    this.api.getAllRecipesApi().subscribe((res: any) => {
+      console.log('All recipes in recipe page');
+      console.log(res);
+      this.allRecipes = res;
+      this.allRecipesDummy = res;
+      this.allRecipes.forEach((item: any) => {
+        !this.cuisineArray.includes(item.cuisine) && //checking if there exist that particular cuisine item in cuisineArray
+          this.cuisineArray.push(item.cuisine);
+      });
+      console.log('all cuisins');
+      console.log(this.cuisineArray);
+
+      //create mealsArray
+      this.allRecipes.forEach((item: any) => {
+        let tempMeal = item.mealType; //['Lunch', 'Dinner']
+        if (tempMeal && tempMeal.length > 0) {
+          tempMeal.forEach((meal: any) => {
+            !this.mealsArray.includes(meal) && this.mealsArray.push(meal);
+          });
+        }
+      });
+      console.log('Meals Array');
+      console.log(this.mealsArray);
+    });
+  }
+
+  filterAllRecipes(value: string) {
+    //alert(value)
+    this.allRecipes = this.allRecipesDummy.filter((item: any) =>
+      item['cuisine'].includes(value)
+    );
   }
 
 
-  getAllRecipes(){
-     this.api.getAllRecipesApi().subscribe((res:any)=>{
-    console.log("All recipes in recipe page");
-    console.log(res);
-    this.allRecipes=res;
-    
-  })
-}
+ filterByMealType(value:string){
+  this.allRecipes = this.allRecipesDummy.filter((item: any) =>
+      item['mealType'].includes(value)
+    );
 
+ }
 }
